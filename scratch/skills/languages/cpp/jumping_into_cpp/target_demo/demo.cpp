ac8ebd47f6c45c01f389bca57adb9650d8b237ef
Rob Gillen <ru7@ornl.gov>
2022-04-29 08:23:33 -0400

    Working on demo app

--- scratch/skills/languages/cpp/jumping_into_cpp/target_demo/demo.cpp
+++ scratch/skills/languages/cpp/jumping_into_cpp/target_demo/demo.cpp
@@ -1,17 +1,82 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
+#include <chrono>
+#include <ctime>
 #include "getopt.h"
+#include "glog/logging.h"
+#include <map>
 
 using namespace std;
+using namespace std::chrono;
 
+
+// [2022-04-28 19:39:24,347] INFO 70.20
+enum LogLevel { DEBUG, INFO, WARNING, ERROR };
+
+static std::map< LogLevel, const char * > LogLevelStrings = {
+   {DEBUG, "DEBUG"},
+   {INFO, "INFO"},
+   {WARNING, "WARNING"},
+   {ERROR, "ERROR"}
+};
+
+static std::map< LogLevel, const char * > LogLevelColors = {
+   {DEBUG, "\033[1;34m"},
+   {INFO, "\033[1;32m"},
+   {WARNING, "\033[1;33m"},
+   {ERROR, "\033[1;31m"}
+};
+
+void log(LogLevel level, string message) {
+    time_t t = system_clock::to_time_t(system_clock::now());
+    tm * my_tm = localtime(&t);
+
+    cout << "["
+         << setfill('0')
+         << setw(4) << 1900 + my_tm->tm_year << "-"
+         << setw(2) << 1 + my_tm->tm_mon << "-"
+         << setw(2) << my_tm->tm_mday << " "
+         << setw(2) << my_tm->tm_hour << ":"
+         << setw(2) << my_tm->tm_min << ":"
+         << setw(2) << my_tm->tm_sec
+         << "] " << LogLevelColors[level] 
+         << LogLevelStrings[level] << " "
+         << message 
+         << "\033[0m" << endl;
+}
+
+
+
+// https://stackoverflow.com/questions/52467531/using-getopt-in-c-to-handle-arguments
+
+// Primary entry point for the demo application
 int main(int argc, char* argv[]) {
-    option longopts[] = {
-        {"number", optional_argument, NULL, 'n'},
-        {"show-ends", optional_argument, NULL, 'E'},
-        {0}
-    };
+    log(INFO, "** My Sample Application Starting **");
+    system_clock::time_point start = system_clock::now();
+
+    // option longopts[] = {
+    //     {"number", optional_argument, NULL, 'n'},
+    //     {"show-ends", optional_argument, NULL, 'E'},
+    //     {0}
+    // };
+
+    // do the thing
+    log(DEBUG, "do the thing");
+    log(INFO, "do the thing");
+    log(WARNING, "do the thing");
+    log(ERROR, "do the thing");
 
 
+    system_clock::time_point end = system_clock::now();   
+    duration<double> elapsed_seconds = end-start;
+    log(INFO, "Program Finished"); 
 
+    // update the user
+    ostringstream ss;
+    ss << "Elapsed Time: " << elapsed_seconds.count() << " seconds";
+    log(INFO, ss.str());
+
+    // indicate success
     return 0;
 }
---
+++ scratch/skills/languages/cpp/jumping_into_cpp/target_demo/demo.md
@@ -0,0 +1,20 @@
+# Demonstration File
+
+The goal of this program is to be a very simple "starter" for some C++ based applications. The intent is to replace what I've been doing in Python with a standard C++ variant.
+
+The program needs to:
+
+- set up logging
+- provide info out to the user
+- parse command line arguments
+- time the overall execution and report
+- minimize the dependence on 3rd-party libraries
+
+```bash
+# set up google logging (?)
+brew install glog
+
+
+# GLOG_logtostderr=1 ./your_application
+
+```
